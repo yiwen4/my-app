9fccbbcf2e2a4bbcf64529008b2b307c76e63bfd
yiwen <taiyangyiwen@gmai.com>
2022-09-13 19:42:59 +0800

    again

--- gituse.js
+++ gituse.js
@@ -1,1 +1,7 @@
-//git init     把这个目录变成Git可以管理的仓库
+//git init                 把这个目录变成Git可以管理的仓库
+//把一个文件放到Git仓库
+//1. git add               把文件添加到仓库
+//2.git commit             告诉Git，把文件提交到仓库
+//-m                       后面输入的是说明
+
+//git status               掌握仓库当前的状态
