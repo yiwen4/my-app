7e7cdd8fe3211fb7df4d709568e61d434fa93f26
yiwen <taiyangyiwen@gmai.com>
2022-09-13 19:53:24 +0800

    andd agin

--- gituse.js
+++ gituse.js
@@ -1,7 +1,15 @@
-//git init                 把这个目录变成Git可以管理的仓库
+//git init                  把这个目录变成Git可以管理的仓库
 //把一个文件放到Git仓库
-//1. git add               把文件添加到仓库
-//2.git commit             告诉Git，把文件提交到仓库
-//-m                       后面输入的是说明
+//1. git add                把文件添加到仓库
+//2.git commit              告诉Git，把文件提交到仓库
+//-m                        后面输入的是说明
 
-//git status               掌握仓库当前的状态
+//git status                掌握仓库当前的状态
+//git diff                  查看和上一个版本的不同
+//git log                   查看历史提交日志
+//git log --pretty=oneline  同上 精简版
+
+//HEAD                      当前版本
+//HEAD^                     上一个版本就是   
+//HEAD^^                    上上一个版本
+//HEAD~100                  就是当然往上100个版本 
